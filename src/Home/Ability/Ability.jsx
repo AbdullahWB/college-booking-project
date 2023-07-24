b8c81a5333adb30604784167df2f2d5356603fc0
AbdullahWB <abdullah917828@gmail.com>
2023-07-24 23:17:49 +0600

    make a ability part

---
+++ src/Home/Ability/Ability.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Container from '../../Shared/Container';
+
+const Ability = () => {
+    return (
+        <Container>
+            <div className='grid grid-cols-4 gap-10 rounded-xl mt-[150px] py-10 px-5 bg-primary bg-opacity-10'>
+                <div className='text-center p-5 '>
+                    <h1 className='text-primary text-6xl font-bold'>65+</h1>
+                    <h3 className='text-secondary text-2xl font-bold my-3'>Branch Totals</h3>
+                    <p className='text-gray-500 text-xl font-semibold'>Lorem ipsum dolor</p>
+                </div>
+                <div className='text-center p-5 '>
+                    <h1 className='text-primary text-6xl font-bold'>350+</h1>
+                    <h3 className='text-secondary text-2xl font-bold my-3'>Our Team</h3>
+                    <p className='text-gray-500 text-xl font-semibold'>Lorem ipsum dolor</p>
+                </div>
+                <div className='text-center p-5 '>
+                    <h1 className='text-primary text-6xl font-bold'>160+</h1>
+                    <h3 className='text-secondary text-2xl font-bold my-3'>Our Programs</h3>
+                    <p className='text-gray-500 text-xl font-semibold'>Lorem ipsum dolor</p>
+                </div>
+                <div className='text-center p-5 '>
+                    <h1 className='text-primary text-6xl font-bold'>35+</h1>
+                    <h3 className='text-secondary text-2xl font-bold my-3'>Our Classes</h3>
+                    <p className='text-gray-500 text-xl font-semibold'>Lorem ipsum dolor</p>
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+export default Ability;
--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Ability from '../Ability/Ability';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Ability />
         </div>
     );
 };
